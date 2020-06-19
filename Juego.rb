dfd3bab833c1ed70c380361d88103c5ef349e531
PaoSantollo <44455124+PaoSantollo@users.noreply.github.com>
2020-06-19 17:26:15 -0500

    Clean code
    clean my code, delete the variables xAuxiliar and yAuxiliar,implement the next move

--- Juego.rb
+++ Juego.rb
@@ -13,54 +13,37 @@ class Juego
     for y in 1..8
       for x in 1..8
         contador_vecino = 0
-        xAuxiliar = x
-        yAuxiliar = y + 1
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x][y + 1] == 1
           contador_vecino += 1
         end
-        xAuxiliar = x - 1
-        yAuxiliar = y + 1
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x - 1][y + 1] == 1
           contador_vecino += 1
         end
-        xAuxiliar = x + 1
-        yAuxiliar = y + 1
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x + 1][y + 1] == 1
           contador_vecino += 1
         end
-        xAuxiliar = x + 1
-        yAuxiliar = y
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x + 1][y] == 1
           contador_vecino += 1
         end
-        xAuxiliar = x - 1
-        yAuxiliar = y
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x - 1][y] == 1
           contador_vecino += 1
         end
-        xAuxiliar = x - 1
-        yAuxiliar = y - 1
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x - 1][y - 1] == 1
           contador_vecino += 1
         end
-        xAuxiliar = x
-        yAuxiliar = y - 1
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x][y - 1] == 1
           contador_vecino += 1
         end
-        xAuxiliar = x + 1
-        yAuxiliar = y - 1
-        if tablero[xAuxiliar][yAuxiliar] == 1
+        if tablero[x + 1][y - 1] == 1
           contador_vecino += 1
         end
-
-        if contador_vecino == 1 || contador_vecino == 0 || contador_vecino >= 4
+        if contador_vecino != 3 || contador_vecino != 2
           tablero_aux[x][y] = 0
         end
         if contador_vecino == 3 || contador_vecino == 2
           tablero_aux[x][y] = 1
         end
-        #print x, "x", y, "y", contador_vecino, "cont \n"
+        #print "con", contador_vecino, "x", x, "y", y, "\n"
       end
     end
   end
@@ -88,9 +71,11 @@ tablero = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 tablero_aux = Array.new(10) { Array.new(10) { 0 } }
-
 juego = Juego.new
-juego.imprimir_tablero(tablero)
-juego.reglas(tablero, tablero_aux)
-juego.clonar_tablero(tablero, tablero_aux)
-juego.imprimir_tablero(tablero)
+
+for i in 0..9
+  print "juego ", i, "\n"
+  juego.imprimir_tablero(tablero)
+  juego.reglas(tablero, tablero_aux)
+  juego.clonar_tablero(tablero, tablero_aux)
+end
